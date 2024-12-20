0080ba5297a566aa2ce4f37e292ab26087eca4c1
jonadableite <jonadab.leite@gmail.com>
2024-12-20 19:58:43 -0300

    prisma

--- src/app.js
+++ src/app.js
@@ -74,7 +74,7 @@ app.use("/dashboard", authMiddleware, dashboardRoutes);
 
 const PORT = process.env.PORT || 3050;
 app.listen(PORT, () => {
-	console.log(`Servidor rodando na porta ${PORT}`);
+	console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
 
 export { prisma, stripeInstance };
